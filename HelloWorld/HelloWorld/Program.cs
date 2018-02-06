3c55a4317370dc26b322697f6648b1616be6e202
kraulk <kristofer.kraul@gmail.com>
2018-02-06 10:16:59 -0500

    Adds personlized HelloWorld solution

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.ReadLine();
+        }
+    }
+}
