9e39d16e5d68b26097263c63cc5e523c7f0684ac
kraulk <kristofer.kraul@gmail.com>
2018-02-06 20:00:54 -0500

    Complets class 3.1 studio

---
+++ Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            double radius;
+            double area;
+
+            Console.WriteLine("Enter a radius: ");
+            radius = double.Parse(Console.ReadLine());
+
+            area = pi * radius * radius;
+
+            Console.WriteLine($"The area of circle with radius {radius} is: {area}.", radius, area);
+            Console.ReadLine();
+        }
+    }
+}
